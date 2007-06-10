c5706d905d2e077956797f8a0df7491454369459
setoppu <setoppu@1de34d1b-782f-0410-a8bc-c64643783fd7>
2007-06-10 04:12:06 +0000

    ShtOp.exeのサスペンド・休止状態への対応。 　※Windows98でのSetSuspendStateのエラーに関しては未対応。

--- Plugins/ExitWin/branches/Suspend and Hibernation Test/ShtOp/ShtOp.dpr
+++ Plugins/ExitWin/branches/Suspend and Hibernation Test/ShtOp/ShtOp.dpr
@@ -8,18 +8,39 @@ uses
 {$R *.RES}
 {$R SHTOP2.RES}
 
+type
+  TIsPwrSuspendAllowed = function(): BOOL;stdcall;
+  TIsPwrHibernateAllowed = function(): BOOL;stdcall;
+  TSetSuspendState = function(Hibernate : BOOL; ForceCritical : BOOL; DisableWakeEvent : BOOL) : Boolean; stdcall;
+
 var
   UpParam: String;
   CanExit: Boolean;
   ExtMode: Integer;
+  HibernateMode: Boolean;
+  SuspendFlag: Boolean;
   VerInfo: TOSVersionInfo;
   tpSv: TTokenPrivileges;
   i: Integer;
   w: HWND;
   s: String;
+  hPowrprof : HINST;
+  IsPwrSuspendAllowed : TIsPwrSuspendAllowed;
+  IsPwrHibernateAllowed : TIsPwrHibernateAllowed;
+  SetSuspendState : TSetSuspendState;
 begin
   CanExit := False;
   ExtMode := 0;
+  HibernateMode := False;
+  SuspendFlag := False;
+  w := 0;
+  hPowrprof := LoadLibrary('PowrProf.dll');
+  if hPowrprof <> 0 then
+  begin
+    @IsPwrSuspendAllowed := GetProcAddress(hPowrprof, 'IsPwrSuspendAllowed');
+    @IsPwrHibernateAllowed := GetProcAddress(hPowrprof, 'IsPwrHibernateAllowed');
+    @SetSuspendState := GetProcAddress(hPowrprof, 'SetSuspendState');
+  end;
   for i := 1 to ParamCount do
   begin
     UpParam := UpperCase(ParamStr(i));
@@ -52,12 +73,31 @@ begin
     begin
       ExtMode := ExtMode or EWX_FORCEIFHUNG;
       CanExit := True;
-    end;
-
-
+    end
+    else if UpParam = 'SUSPEND' then
+    begin
+      if Assigned(@IsPwrSuspendAllowed) and IsPwrSuspendAllowed() then
+      begin
+        HibernateMode := False;
+        SuspendFlag := True;
+      end;
+    end
+    else if UpParam = 'HIBERNATION' then
+    begin
+      if Assigned(@IsPwrHibernateAllowed) and IsPwrHibernateAllowed() then
+        begin
+          HibernateMode := True;
+          SuspendFlag := True;
+        end;
+      end;
   end;
 
-  if CanExit then
+  if SuspendFlag and Assigned(@SetSuspendState) then
+  begin
+    // サスペンド処理を追加
+    SetSuspendState(HibernateMode, False, False);
+  end
+  else if CanExit then
   begin
     VerInfo.dwOSVersionInfoSize := SizeOf(TOSVersionInfo);
 
@@ -95,6 +135,26 @@ begin
       end;
     end;
 
-    if VerInfo.dwPlatformId = VER_PLATFORM_WIN32_NT then      ReleaseSE_SHUTDOWN(tpSv);  end  else  begin    s := 'Windows の終了を行います。' + #13#10       + #13#10       + 'ShtOp [shutdown|reboot|logoff|poweroff] [force|forceifhung]' + #13#10       + #13#10       + 'shutdown   コンピュータをシャットダウンします。'+ #13#10       + 'reboot   コンピュータを再起動します。' + #13#10       + 'logoff   Windowsからログオフします。' + #13#10       + 'poweroff   コンピュータをシャットダウンし､ 電源を切ります。' + #13#10       + #13#10       + 'force   プロセスを強制的に終了させます。' + #13#10       + 'forceifhung   プロセスが反応がない場合に強制的に終了させます。(Windows 2000 のみ有効）';    MessageBox(0, PChar(s), '情報', MB_ICONINFORMATION);  end;end.
+    if VerInfo.dwPlatformId = VER_PLATFORM_WIN32_NT then
+      ReleaseSE_SHUTDOWN(tpSv);
+  end
+  else
+  begin
+    s := 'Windows の終了を行います。' + #13#10
+       + #13#10
+       + 'ShtOp [shutdown|reboot|logoff|poweroff|suspend|hibernation] [force|forceifhung]' + #13#10
+       + #13#10
+       + 'shutdown' + #09#09 + 'コンピュータをシャットダウンします。'+ #13#10
+       + 'reboot'+ #09#09 + 'コンピュータを再起動します。' + #13#10
+       + 'logoff' + #09#09 + 'Windowsからログオフします。' + #13#10
+       + 'poweroff' + #09#09 + 'コンピュータをシャットダウンし､ 電源を切ります。' + #13#10
+       + 'suspend' +#09#09 + 'コンピュータをサスペンド状態に移行します。' + #13#10
+       + 'hibernation' + #09 + 'コンピュータを休止状態に移行します。' + #13#10
+       + #13#10
+       + 'force' + #09#09 + 'プロセスを強制的に終了させます。' + #13#10
+       + 'forceifhung' + #09 + 'プロセスが反応がない場合に強制的に終了させます。(Windows 2000 のみ有効）';
+    MessageBox(0, PChar(s), '情報', MB_ICONINFORMATION);
+  end;
+end.
 
 
